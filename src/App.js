01eaf7406d23f035b282461918df9c210c862669
Ben Straub <ben@straub.cc>
2018-01-08 20:59:28 -0800

    Starting a skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Row, Col } from "react-flexbox-grid";
+import { Search } from './search';
+
+const AppCol = ({children}) => (
+  <Col xs={12} md={8} mdOffset={2}>
+    {children}
+  </Col>
+)
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Grid fluid>
+        <AppCol>
+        <Row>
+          <Col xs>
+            <h1 style={{'text-align': 'center'}}>Weights</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs>
+            <Row>
+              <label htmlFor="movement">What are we lifting today?</label>
+            </Row>
+            <Row>
+              <Search />
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs>(search results)</Col>
+        </Row>
+        </AppCol>
+      </Grid>
     );
   }
 }
---
+++ src/search.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from "react-flexbox-grid";
+
+export const Search = props => (
+  <input type="text" id="movement" placeholder="Movement"
+    style={{ width: '100%' }} />
+)
