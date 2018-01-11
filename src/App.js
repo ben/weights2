057aebf1374fe34f54594320b81fc18782aa3873
Ben Straub <ben@straub.cc>
2018-01-11 06:14:00 -0800

    nvm, use Skeleton

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from "react-flexbox-grid";
 import { Search } from './search';
-
-const AppCol = ({children}) => (
-  <Col xs={12} md={8} mdOffset={2}>
-    {children}
-  </Col>
-)
 
 class App extends Component {
   render() {
     return (
-      <Grid fluid>
-        <AppCol>
-        <Row>
-          <Col xs>
-            <h1 style={{'text-align': 'center'}}>Weights</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs>
-            <Row>
-              <label htmlFor="movement">What are we lifting today?</label>
-            </Row>
-            <Row>
-              <Search />
-            </Row>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs>(search results)</Col>
-        </Row>
-        </AppCol>
-      </Grid>
-    );
+      <div className="container">
+        <section className="header">
+          <h1>Weights</h1>
+        </section>
+        <Search />
+        (search results)
+      </div>
+    )
   }
 }
 
