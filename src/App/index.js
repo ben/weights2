7cba5648e476d1faab8c6f98fba331fc5bf3fc4d
Ben Straub <ben@straub.cc>
2018-01-11 06:22:16 -0800

    Move components into directories

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { Search } from './search';
+import { Search } from '../Search';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ''
+    }
+  }
+
+  setSearch = search => {
+    this.setState({
+      search
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <section className="header">
           <h1>Weights</h1>
         </section>
-        <Search />
+        <Search text={this.state.search} onChange={this.setSearch} />
         (search results)
       </div>
     )
--- src/search.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { Grid, Row, Col } from "react-flexbox-grid";
-
-export const Search = props => (
-  <input type="text" id="movement" placeholder="Movement"
-    style={{ width: '100%' }} />
-)
